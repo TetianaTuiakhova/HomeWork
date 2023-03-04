341347c7fa1f23d34d3ea60fcfb7379d502c8e80
TetianaTuiakhova <125880243+TetianaTuiakhova@users.noreply.github.com>
2023-03-04 22:07:35 +0000

    HomeWork04.03.2023
    1 уровень сложности: Задание 1
    установить JDK 11, 17
    создать два проекта Java, переключить в настройках один на 11, второй на 17
    Задание 2
    В любом из проектов создать несколько разных пакетов
    В каждом пакете создать класс(Java-файл), который при запуске программы выводит сообщения "Привет. Я -  подставить название класса"

---
+++ Home Java 04.03.2023/Java1/src/Basic1/Level1/Java1/Hello.java
@@ -0,0 +1,5 @@
+package Basic1.Level1.Java1;
+
+public class Hello {
+    public int number = 8;
+}
---
+++ Home Java 04.03.2023/Java1/src/Main.java
@@ -0,0 +1,8 @@
+import Basic1.Level1.Java1.Hello;
+
+public class Main {
+    public static void main(String[] args) {
+        Hello sun = new Hello();
+        System.out.println ("Привет. Я -"+sun.number);
+    }
+}
