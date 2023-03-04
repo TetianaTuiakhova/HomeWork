a1cba79ba6ac3269f8c049aa2ceaf1cf283a80dd
TetianaTuiakhova <125880243+TetianaTuiakhova@users.noreply.github.com>
2023-03-04 21:54:30 +0000

    Delete Java1 directory

--- Java1/src/Basic1/Level1/Java1/Hello.java
+++
@@ -1,5 +0,0 @@
-package Basic1.Level1.Java1;
-
-public class Hello {
-    public int number = 8;
-}
--- Java1/src/Main.java
+++
@@ -1,8 +0,0 @@
-import Basic1.Level1.Java1.Hello;
-
-public class Main {
-    public static void main(String[] args) {
-        Hello sun = new Hello();
-        System.out.println ("Привет. Я -"+sun.number);
-    }
-}
