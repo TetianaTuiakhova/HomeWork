62209a7e031087857f44900ddb6ffe5908c70374
TetianaTuiakhova <125880243+TetianaTuiakhova@users.noreply.github.com>
2023-03-22 13:55:17 +0300

    Homework01.03.2023
     1 уровень сложности: Задание 1:
    Создать переменные типа boolean, int, String, double.
    Каждой переменной задать значение в соответствии с этим типом.
    Вывести значение каждой переменной на экран. 
    Задание 2:
    Написать 4 метода в любом классе каждый из которых соответствует определённо математической операции(+, -, /, *) - сложение, вычитание, деление, умножение. 
    В каждом методе две целочисленные переменные должны быть созданы с определённым значением, с ними должна быть проведена математическая операция, соответствующая  названию метода и выведен результат.
    Эти методы должны быть вызваны из метода "main".

---
+++ Home Java 01.03.2023/Main/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 8;
+        double numberDouble = 3.7;
+        String string = "Hello, world";
+        boolean booleant = true;
+
+        System.out.println(number);
+        System.out.println(numberDouble);
+        System.out.println(string);
+        System.out.println(booleant);
+    }
+}
---
+++ Home Java 01.03.2023/Main2/src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        add();
+        subtract();
+        divide();
+        multiply();
+    }
+    public static void add() {
+        int number1 = 2;
+        int number2 = 8;
+
+        int result = number2 + number1;
+
+        System.out.println(result);
+
+    }
+    public static void subtract() {
+        int number1 = 2;
+        int number2 = 8;
+
+        int result = number2 - number1;
+
+        System.out.println(result);
+
+    }
+    public static void divide() {
+        int number1 = 2;
+        int number2 = 8;
+
+        int result = number2 / number1;
+
+        System.out.println(result);
+
+    }
+    public static void multiply() {
+        int number1 = 2;
+        int number2 = 8;
+
+        int result = number2 * number1;
+
+        System.out.println(result);
+
+    }
+}
